29c980be1ceb45525db512409d335fbe22948f8c
bsdpunk <bsdpunk@gmail.com>
2019-04-25 17:26:26 -0500

    AddedSomeJsonMetaData

--- egrepFun.sh
+++ egrepFun.sh
@@ -25,16 +25,42 @@ function csvcount () { head -n1 "$@" | grep -o , | tr -d "\n" | wc -c; }
 function prepCsv() {
     file=$1
     csvcols=$( csvcount $file | tr -d ' \t')
+    head -n1 $file | rqc > header
     #echo $csvcols
     for i in $(seq 1 $csvcols)
     do
         rqc $1 | awk -F, -v colnumber=$i '{print $colnumber}'  | tail -n+2 > col$i
         prepMetaData $i
+        prepJson $i > metaData$1.json
     done
 }
 function prepMetaData() {
-    awk '{ print length }' col$1 | sort | uniq -c > collength$1
+prepLengthData $1
 }
+function prepLengthData() {
+
+    awk '{ print length }' col$1 | sort | uniq -c | sort -rnk2 > collength$1
+}
+function prepJson() {
+       
+    max=$(head -n1 collength$1 | gsed 's/^[ 0-9]\+ /"max": "/' | sed 's/$/",/')
+    min=$(tail -n1 collength$1 | gsed 's/^[ 0-9]\+ /"min": "/' | sed 's/$/",/')
+    cname=$(awk -F, -v name="$1" '{print $1}' header)
+    totalCols=$(echo "\"total\": \"\"")
+    echo "{"
+    echo '  "metadata":'
+    echo "    ["
+    echo "      {"
+    echo "        \"column\": \"$cname\","
+    echo "        \"length\": {"
+    echo "          $max"
+    echo "          $min"
+    echo "          $totalCols"
+    echo "          }"
+    echo "}]}"
+
+}
+
 function dataTypesCsv() {
 
     file=$1
