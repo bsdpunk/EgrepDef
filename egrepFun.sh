e0d5aa8d6c588fbdcd0cb82e2761462f719e5cbd
bsdpunk <bsdpunk@gmail.com>
2019-04-25 21:09:46 -0500

    Got metaData.json Working, Improved JSON structure, improved general JSON files.

--- egrepFun.sh
+++ egrepFun.sh
@@ -19,89 +19,146 @@
 
 ##set -o nounset                              # Treat unset variables as an error
 
-
+one=1
 function csvcount () { head -n1 "$@" | grep -o , | tr -d "\n" | wc -c; }
 
 function prepCsv() {
     file=$1
     csvcols=$( csvcount $file | tr -d ' \t')
+    twoCsvcols=$((csvcols += 1))
     head -n1 $file | rqc > header
     #echo $csvcols
-    for i in $(seq 1 $csvcols)
+    rm -rf cols
+
+    echo "{" > metaData.json
+    echo "\"columns\": [" >> metaData.json
+    for i in $(seq 1 $twoCsvcols)
     do
         rqc $1 | awk -F, -v colnumber=$i '{print $colnumber}'  | tail -n+2 > col$i
-        prepMetaData $i
-        prepJson $i > metaData$1.json
+        prepMetaData $i 
+        dataTypesCsv $i >> cols
+        prepJson $i > metaData$i.json
+        prepJson $i >> metaData.json
+        echo "," >> metaData.json
     done
+    sed -i '$s/,$//' metaData.json
+    echo "]}" >> metaData.json
+
 }
+
 function prepMetaData() {
-prepLengthData $1
+    prepLengthData $1
 }
+
 function prepLengthData() {
 
     awk '{ print length }' col$1 | sort | uniq -c | sort -rnk2 > collength$1
 }
+
 function prepJson() {
-       
+
     max=$(head -n1 collength$1 | gsed 's/^[ 0-9]\+ /"max": "/' | sed 's/$/",/')
     min=$(tail -n1 collength$1 | gsed 's/^[ 0-9]\+ /"min": "/' | sed 's/$/",/')
-    cname=$(awk -F, -v name="$1" '{print $1}' header)
+    cname=$(awk -F, -v name="$1" '{print $name}' header)
+    dtype=$( grep "Col $i" cols | egrep -o "[A-Za-z]+$"  | tail -n1)
     totalCols=$(echo "\"total\": \"\"")
     echo "{"
-    echo '  "metadata":'
-    echo "    ["
+    echo "  \"column\": \"$cname\","
+    echo "    \"data\": ["
     echo "      {"
     echo "        \"column\": \"$cname\","
+    echo "        \"columnNumber\": \"$1\","
+    echo "        \"dataType\": \"$dtype\","
     echo "        \"length\": {"
     echo "          $max"
     echo "          $min"
     echo "          $totalCols"
     echo "          }"
-    echo "}]}"
+    echo "     }"
+    echo "   ]"
+    echo "}"
 
 }
 
 function dataTypesCsv() {
+    i=$1
 
-    file=$1
-    csvcols=$( csvcount $file | tr -d ' \t')
-    #echo $csvcols
-    total=$(wc -l col$i | tr -d ' \t')
-    for i in $(seq 1 $csvcols)
-    do
-        #echo $i
-        #echo $csvcols
+    total=$(  wc -l col$i | egrep -o '[0-9]+' |  head -n1 |tr -d ' \t')
+    if isnumber col$i "10" ; then
+        if isnumber col$i $total; then
+            echo "Col $i is integer"
+        fi
+    elif haswhitespace col$i 10; then
+        if isdatetime col$i 10; then
 
-        number=$( isnumber col$i | tr -d ' \t') 
-        #echo $number
-        #echo $total
-        if [[ "$number" -eq "$total" ]]; then
-            echo "Col $i is number"
-        else
-
-                hw=$( haswhitespace <( head col$i) | tr -d ' \t' )
-            if [[ "$hw" -eq 9 ]]; then
-                dtTest=$( isdatetime <( head col$i) | tr -d ' \t' )
-                #echo $dtTest
-                if [[ "$dtTest" -eq 9 ]]; then
-
-                    dt=$( isdatetime col$i | tr -d ' \t' )
-                    if [[ "$dt" -eq "$total" ]]; then
-                        echo "Col $i is DateTime"
-                    fi
-                fi
+            if isdatetime col$i $total; then
+                echo "Col $i is DateTime"
             fi
         fi
-    done
+    elif haspunc col$i 10; then
+        echo "Col $i Has Punc"
+    fi
 }
 
 function isnumber() {
-    egrep -c "^[0-9,\.]+$" $1    
+
+    if [[ "$2" -eq 10 ]]; then 
+
+        count=$(egrep -c "^[0-9,\.]+$" <( head $1) | tr -d ' \t')
+    else
+
+        count=$(egrep -c "^[0-9,\.]+$" $1  | tr -d ' \t')
+    fi
+    if [[ "$count" -eq "$2" ]]; then
+        return 0
+    else
+        return 1
+    fi
+
 }
 function haswhitespace() {
-    egrep -c "( |\t)" $1
+    if [[ "$2" -eq 10 ]]; then 
+
+        count=$(egrep -c "( |\t)" <( head $1) | tr -d ' \t')
+    else
+
+        count=$(egrep -c "( |\t)" $1  | tr -d ' \t')
+    fi
+    if [[ "$count" -gt 0 ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+function haspunc() {
+    if [[ "$2" -eq 10 ]]; then 
+
+        count=$(egrep -c "[:punct:\.]" <( head $1) | tr -d ' \t')
+    else
+
+        count=$(egrep -c "[:punct:\.]" $1  | tr -d ' \t')
+    fi
+    if [[ "$count" -gt 0 ]]; then
+        return 0
+    else
+        return 1
+    fi
 }
 
 function isdatetime() {
-    egrep -c "^[0-9]{4,4}-[0-1]{1,1}[0-9]{1,1}-[0-3]{1,1}[0-9]{1,1} [0-2]{1,1}[0-9]{1,1}:[0-5]{1,1}[0-9]{1,1}:[0-5]{1,1}[0-9]$" $1
+
+    if [[ "$2" -eq 10 ]]; then 
+        count=$(egrep -c "^[0-9]{4,4}-[0-1]{1,1}[0-9]{1,1}-[0-3]{1,1}[0-9]{1,1} [0-2]{1,1}[0-9]{1,1}:[0-5]{1,1}[0-9]{1,1}:[0-5]{1,1}[0-9]$" <( head $1) )
+    else
+        count=$(egrep -c "^[0-9]{4,4}-[0-1]{1,1}[0-9]{1,1}-[0-3]{1,1}[0-9]{1,1} [0-2]{1,1}[0-9]{1,1}:[0-5]{1,1}[0-9]{1,1}:[0-5]{1,1}[0-9]$" $1)
+    fi
+    if [[ "$count" -eq "$2" ]]; then
+        return 0
+    else
+        return 1
+    fi
+
+
+
 }
